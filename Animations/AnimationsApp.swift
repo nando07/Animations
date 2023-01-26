0c151e746ee2cec3df8b3b825056c5d5ac543f18
Fernando Gomez <redchameleonapps@gmail.com>
2023-01-25 23:16:02 -0700

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Fernando Gomez on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
