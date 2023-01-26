1d93ed04d05dbaf9c14fdde4d7bf265b6a007679
Fernando Gomez <redchameleonapps@gmail.com>
2023-01-25 23:57:09 -0700

    Added a button coin turning 360 degrees with spring effect.

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,24 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
 
     var body: some View {
-        print(animationAmount)
-        return VStack {
-            Stepper("Scale amount", value: $animationAmount.animation(.easeOut(duration: 1).repeatCount(3, autoreverses: true)), in: 1...10)
-            
-            Spacer()
-            
-            Button("Tap Me") {
-                animationAmount += 1
+        
+        
+        // Coin rotating 360 degrees with spring effect
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
             }
-            .padding(50)
-                  .background(.red)
-            .foregroundColor((.white))
-                .clipShape(Circle())
-                .scaleEffect(animationAmount)
         }
+                 // do nothing
+             .padding(50)
+             .background(.red)
+             .foregroundColor(.white)
+             .clipShape(Circle())
+             .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+
+         
+        
+        
+//        print(animationAmount)
+//        return VStack {
+//            Stepper("Scale amount", value: $animationAmount.animation(.easeOut(duration: 1).repeatCount(3, autoreverses: true)), in: 1...10)
+//
+//            Spacer()
+//
+//            Button("Tap Me") {
+//                animationAmount += 1
+//            }
+//            .padding(50)
+//                  .background(.red)
+//            .foregroundColor((.white))
+//                .clipShape(Circle())
+//                .scaleEffect(animationAmount)
+//        }
         
 //        Button("Tap Me") {
 ////            animationAmount += 1
